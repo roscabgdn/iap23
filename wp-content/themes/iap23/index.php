e1da856097d24c4610ca9b4f2b03bff9dd42f0c4
Rosca Bogdan <bogdan.rosca@rbpixel.com>
2023-01-21 09:43:55 +0200

    changed index.php theme and added specs

--- wp-content/themes/iap23/index.php
+++ wp-content/themes/iap23/index.php
@@ -4,28 +4,24 @@
  */
 
 get_header(); ?>
-<!-- <div class="container">
-    <div class="card">
-        <div class="card__title">
-            Random
-        </div>
-        <div class="card__content">
-            <div class="card__meta">
-                by <a href="#">author</a> in <a href="#">category</a>
-            </div>
-            <div class="card__content__title">
-                <a href="#">Mind-Blowing Twitter Stats and Facts on Our Favorite Network (2018)</a>
-            </div>
-            <div class="card__content__content">
-                <p>User research is the reality check every project needs. Here’s our guide to why you should be doing it — and how to get started.</p>
-            </div>
-        </div>
-        <div class="card__image">
-           <a href="#">
-           <img src="https://picsum.photos/500/500" alt="">
-           </a>
-        </div>
+<div class="container">
+    <!-- 
+        Aici o sa implementati card-ul primit in email. 
+        Ce va trebui sa faceti? 
+        1. Creati un fisier scss in folderul assets/scss numit _news_card.scss
+        2. Includeti fisierul scss in style.scss folosind @import (aveti exemple in fisier)
+        3. Sa nu uitati sa porniti live sass compile (indicatii si explicatii in fisierul primit pe email)
+        5. Poza pentru articol o gasiti in assets/img/poza-articol.png
+
+        Cateva mentiuni de implementare: 
+            Poza ar trebui sa fie clickabila, adica sa fie un link pe poza.
+            Poza are colturile rotujite 
+            Dupa by vine un autor si acolo ar trebui sa fie un link, la fel si dupa in o categorie care la fel o sa fie un link
+            Pe titlul articolului de asemenea trebuie sa fie un link.
+    --> 
+    
+    <div class="news_card">
     </div>
-</div> -->
+</div>
 <?php 
 get_footer();
